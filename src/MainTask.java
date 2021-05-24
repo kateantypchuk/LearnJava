79d46702d36056ca6ea3206d4dcab55797c90339
Kateryna Antypchuk <kateryna_antypchuk@epam.com>
2021-05-24 15:45:51 +0300

    Task1 and Task2

---
+++ src/MainTask.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MainTask {
+
+public static void main(String[] args) {
+            //1.Приветствовать любого пользователя при вводе его имени через командную строку
+            Scanner scan = new Scanner(System.in);
+            for (int i = 0; true; i++) {
+                System.out.println("Hello " + scan.nextLine());
+            }
+        }
+}
+
---
+++ src/MainTask2.java
@@ -0,0 +1,6 @@
+class MainTask2 {
+    public static void main(String[] args) {
+        for( int i = args.length - 1 ; i >= 0 ; i--)
+            System.out.println("Reverse : " + args[i]);
+    }
+}
