361a9e6b7a995c15fdd9e575c789b19a5521671a
Kateryna Antypchuk <kateryna_antypchuk@epam.com>
2021-05-24 16:14:24 +0300

    Task3

---
+++ src/mainTask3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+//Вывести заданное количество случайных чисел с переходом и без перехода на новую строку
+public class mainTask3 {
+    public static void main(String[] args) {
+        System.out.println("Задайте количество чисел");
+        Scanner scan = new Scanner(System.in);
+        int value = scan.nextInt();
+        for (int i = 0; i < value; i++) {
+            System.out.println(Math.random());
+        }
+        for (int i = 0; i < value; i++) {
+            System.out.print(Math.random());
+        }
+    }
+}
